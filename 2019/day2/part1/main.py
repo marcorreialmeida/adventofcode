a1f2475cbb89db7537b091a96e4cc23bc32be4cc
Marco Almeida <marco.almeida@sky.uk>
2019-12-05 00:13:30 +0000

    initial commit

---
+++ 2019/day2/part1/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+ip = 0
+program = [1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,13,1,19,1,5,19,23,2,10,23,27,1,27,5,31,2,9,31,35,1,35,5,39,2,6,39,43,1,43,5,47,2,47,10,51,2,51,6,55,1,5,55,59,2,10,59,63,1,63,6,67,2,67,6,71,1,71,5,75,1,13,75,79,1,6,79,83,2,83,13,87,1,87,6,91,1,10,91,95,1,95,9,99,2,99,13,103,1,103,6,107,2,107,6,111,1,111,2,115,1,115,13,0,99,2,0,14,0]
+print("Initially:", program)
+
+while program[ip] != 99:
+    value = (program[program[ip+1]] + program[program[ip+2]]) if program[ip] == 1 else (program[program[ip+1]] * program[program[ip+2]])
+    print("I'm calculating", value, "and placing it at pos", program[program[ip+3]])
+    program[program[ip+3]] = value
+    print(ip, "produced", program)
+    ip += 4
+
+print(program)
