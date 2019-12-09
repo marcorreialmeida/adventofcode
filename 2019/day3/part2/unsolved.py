ddfb360ce7de52ec275e12e3517625c5a121b381
Marco Almeida <marco.almeida@sky.uk>
2019-12-09 23:44:55 +0000

    can't solve it i think its failing to comply with "If a wire visits a position on the grid multiple times, use the steps value from the first time it visits that position when calculating the total value of a specific intersection"

---
+++ 2019/day3/part2/unsolved.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import re
+
+path1 = open('path1.txt').readline().strip().split(",")
+path2 = open('path2.txt').readline().strip().split(",")
+
+pathRegex = r'^(U|D|L|R)(\d+)$'
+central = (0,0)
+directionFunctions = {
+    "R": lambda loc, length: [(loc[0]+i, loc[1]) for i in range(1, length+1)],
+    "L": lambda loc, length: [(loc[0]-i, loc[1]) for i in range(1, length+1)],
+    "U": lambda loc, length: [(loc[0], loc[1]+i) for i in range(1, length+1)],
+    "D": lambda loc, length: [(loc[0], loc[1]-i) for i in range(1, length+1)],
+}
+
+def distance_from_closest(path1, path2):
+    locations1 = locations(path1)
+    locations2 = locations(path2)
+    intersections = (locations1 & locations2) - {central}
+    return min(map(lambda loc: abs(loc[0])+abs(loc[1]), intersections))
+
+def locations(path):
+    locs = [central]
+    for pathItem in path:
+        locs += directionFunctions[pathItem[0]](locs[-1], int(pathItem[1:]))
+    return set(locs)
+
+def pathsToIntersections(path, intersections):
+    stepsToIntersections = {}
+    for p in range(1, len(path)):
+        locsInPartialPath = locations(path[0:p]) - {central}
+        for inters in intersections:
+            if inters in locsInPartialPath:
+                if inters not in stepsToIntersections:
+                    stepsToIntersections.update({inters: len(locsInPartialPath)})
+    return stepsToIntersections
+
+locations1 = locations(path1)
+locations2 = locations(path2)
+intersections = (locations1 & locations2) - {central}
+
+path1StepsToInsters = pathsToIntersections(path1, intersections)
+path2StepsToInsters = pathsToIntersections(path2, intersections)
+print("I guess:", min(map(lambda x: path1StepsToInsters[x] + path2StepsToInsters[x], intersections)))
+# wrong (too high): 301038, 92220, 92218
